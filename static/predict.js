99934f67bfd5e96bdc0cb7298ca912f150027013
devandroidstudio <96157083+devandroidstudio@users.noreply.github.com>
2022-09-27 20:27:58 +0700

    Update predict.js

--- static/predict.js
+++ static/predict.js
@@ -1,13 +1,20 @@
-let imageLoaded = false;
-let image;
+// import * as tf from '@tensorflow/tfjs';
+// import * as tmImage from '@teachablemachine/image';
+
+
 const URL = "https://teachablemachine.withgoogle.com/models/wYGvo9ymg/";
-let maxPredictions;
+
+let imageLoaded = false;
+let image = null;
 $("#image-selector").change(function () {
 	imageLoaded = false;
 	let reader = new FileReader();
 	reader.onload = function () {
 		let dataURL = reader.result;
-		image = dateURL;
+		
+		image = new Image();
+		image.src = dataURL;
+		console.log(image);
 		$("#selected-image").attr("src", dataURL);
 		$("#prediction-list").empty();
 		imageLoaded = true;
@@ -24,9 +31,8 @@ $( document ).ready(async function () {
 	$('.progress-bar').show();
     console.log( "Loading model..." );
 	const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
-    model = await tmImage.load(modelURL, metadataURL);
-	maxPredictions = model.getTotalClasses();
+	const metadataURL = URL + "metadata.json";
+	model = await tmImage.load(modelURL, metadataURL);
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 	modelLoaded = true;
@@ -36,30 +42,25 @@ $("#predict-button").click(async function () {
 	if (!modelLoaded) { alert("The model must be loaded first"); return; }
 	if (!imageLoaded) { alert("Please select an image first"); return; }
 	
-// 	let image = $('#selected-image').get(0);
+	// let image = $('#selected-image').get(0);
 	
-// 	// Pre-process the image
-// 	console.log( "Loading image..." );
-// 	let tensor = tf.browser.fromPixels(image, 3);
-// 		// .resizeNearestNeighbor([224, 224]) // change the image size
-// 		// .expandDims()
-// 		// .toFloat()
-// 		// .reverse(-1); // RGB -> BGR
-	let predictions = await model.model.predictTopK(image);
-	// console.log(predictions);
-	console.log(image);
-// 	let top5 = Array.from(predictions)
-// 		.map(function (p, i) { // this is Array.map
-// 			return {
-// 				probability: p,
-// 				className: TARGET_CLASSES[i] // we are selecting the value from the obj
-// 			};
-// 		}).sort(function (a, b) {
-// 			return b.probability - a.probability;
-// 		}).slice(0, 2);
-
+	// Pre-process the image
+	console.log( "Loading image..." );
+	
+	let predictions = await model.predictTopK(image);
+	console.log(predictions);
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: TARGET_CLASSES[i] // we are selecting the value from the obj
+			};
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+		}).slice(0, 2);
+		console.log( top5);
 	$("#prediction-list").empty();
 	predictions.forEach(function (p) {
-		$("#prediction-list").append(`<li>${p.className}: ${p.probability}</li>`);
+		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toString()}</li>`);
 		});
 });
